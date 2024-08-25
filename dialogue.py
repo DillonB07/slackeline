783ca38f81258a3cf67d2864f646f3bc7d4586d5
Dillon Barnes <dillonb07dev@gmail.com>
2024-08-25 13:39:27 +0100

    recreate pronoun system, add enhancements to dialogue system and dialogue

--- dialogue.py
+++ dialogue.py
@@ -1,53 +1,79 @@
 WELCOME = [
     [{
+        "icon_emoji": ":flyingbirb:",
+        "username": "Bird",
+        "message": "_*SQUAWK SQUAWK* (user_mention) has started the trail_"
+    },
+    {
         "icon_emoji": ":distracteline:",
-        "username": "Maddy",
-        "message": "Excuse me, ma'am\nThe sign out front is busted... is this the trail to the waffle house?"
+        "username": "Madeline",
+        "message": "Excuse me, ma'am\nThe sign out front is busted... is this the trail to the _:sparkles:waffle house:sparkles:_?"
     },
     {
         "icon_emoji": ":grannycreep:",
-        "username": "Granny",
-        "message": "_You're almost there (ma'am/sir/strange person). It's just across the bridge._"
+        "username": "Old Lady",
+        "message": "_You're almost there, (pronouns). It's just across the bridge._",
+        "replacements": [
+            {
+                "replace": "she",
+                "with": "miss"
+            },
+            {
+                "replace": "they",
+                "with": "strange person",
+                "default": True
+            },
+            {
+                "replace": "he",
+                "with": "mister"
+            }
+        ]
     }, {
         "icon_emoji": ":scaredeline:",
-        "username": "Maddy",
+        "username": "Madeline",
         "message": "By the way, you should call someone about your driveway. The ridge collapsed and I nearly died."
     }, {
         "icon_emoji": ":grannylaughing:",
-        "username": "Granny",
+        "username": "Old Lady",
         "message": "_If my \"driveway\" almost did you in, the trail might be a bit much for you._"
     }, {
         "icon_emoji": ":madeline:",
-        "username": "Maddy",
-        "message": "Well, if an old thing like _you_ can survie out here, I think I'll be fine."
+        "username": "Madeline",
+        "message": "Well, if an old thing like _you_ can survive out here, I think I'll be fine."
     }, {
         "icon_emoji": ":grannycreep:",
-        "username": "Granny",
-        "message": "_Suit yourself._"
+        "username": "Old Lady",
+        "message": "_Suit yourself._",
+        "delay": 1
     }, {
         "icon_emoji": ":grannycreep:",
-        "username": "Granny",
-        "message": " _But you should know, you'll find more than waffles at the end of this trail._"
+        "username": "Old Lady",
+        "message": " _But you should know, you'll find more than :sparkles:waffles:sparkles: at the end of this trail._",
+        "delay": 1
     }, {
         "icon_emoji": ":grannycreep:",
-        "username": "Granny",
-        "message": "_This is a strange place._"
+        "username": "Old Lady",
+        "message": "_This is a strange place._",
+        "delay": 1
     }, {
         "icon_emoji": ":grannycreep:",
-        "username": "Granny",
-        "message": "_You might see things._"
+        "username": "Old Lady",
+        "message": "_You might see things._",
+        "delay": 1
     }, {
         "icon_emoji": ":grannylaugh:",
-        "username": "Granny",
-        "message": "_Things you ain't ready to see._"
+        "username": "Old Lady",
+        "message": "_Things you ain't ready to see._",
+        "delay": 2
     }, {
         "icon_emoji": ":sadeline:",
-        "username": "Maddy",
-        "message": "You should seek help, lady."
+        "username": "Madeline",
+        "message": "You should seek help, lady.",
+        "delay": 0.4
     }, {
         "icon_emoji": ":grannylaughing:",
-        "username": "Granny",
-        "message": ""
+        "username": "Old Lady",
+        "message": "\n"
     }
     ]
 ]
--- main.py
+++ main.py
@@ -20,17 +20,28 @@ def handle_member_joined_channel(body, say):
     welcome_seq = WELCOME[0]
     initial_msg = None
     for msg in welcome_seq:
-        new_msg =""
+        new_msg = msg["message"]
 
-        if "she" in pronouns.lower():
-            new_msg = msg["message"].replace("(ma'am/sir/strange person)", "miss")
-        elif "he" in pronouns.lower():
-            new_msg = msg["message"].replace("(ma'am/sir/strange person)", "sir")
-        else:
-            new_msg = msg["message"].replace("(ma'am/sir/strange person)", "strange person")
-        msg = say(new_msg, icon_emoji=msg["icon_emoji"], username=msg["username"], thread_ts=initial_msg)
+        if "(pronouns)" in new_msg.lower():
+            replaced = False
+            for replacement in msg.get("replacements", []):
+                if replacement["replace"] in pronouns:
+                    new_msg = new_msg.replace("(pronouns)", replacement["with"])
+                    replaced = True
+                    break
+
+            if not replaced:
+                default = next((replacement["with"] for replacement in msg.get("replacements", []) if replacement.get("default")), None)
+                new_msg = new_msg.replace("(pronouns)", default)
+
+        if "user_mention" in new_msg.lower():
+            new_msg = new_msg.replace("(user_mention)", f"<@{body['event']['user']}>")
+
+        sent_msg = say(text=new_msg, icon_emoji=msg["icon_emoji"], username=msg["username"], thread_ts=initial_msg)
+
         if initial_msg is None:
-            initial_msg = msg["ts"]
+            initial_msg = sent_msg["ts"]
+
         time.sleep(msg.get("delay", 1.8))
 
 
